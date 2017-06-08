25bf7717e01fe406494463488e8b9bdd363c2798
ELITE <eliteofindia@gmail.com>
2017-06-08 15:25:22 +0800

    added the change to change the packaging to war and deployable to apache

--- desk/src/main/java/org/service/desk/ServiceDeskAPI.java
+++ desk/src/main/java/org/service/desk/ServiceDeskAPI.java
@@ -5,14 +5,21 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.*;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"org.service.desk.swagger.config","org.service.desk.controllers"})
-public class ServiceDeskAPI {
+public class ServiceDeskAPI extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServiceDeskAPI.class);
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(ServiceDeskAPI.class, args);
     }
