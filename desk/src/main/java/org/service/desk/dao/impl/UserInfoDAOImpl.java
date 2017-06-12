5f611358ca7e1434841d3bcc1817b15158d887de
ELITE <eliteofindia@gmail.com>
2017-06-12 17:49:43 +0800

    added abstraction in Repository

--- desk/src/main/java/org/service/desk/dao/impl/UserInfoDAOImpl.java
+++ desk/src/main/java/org/service/desk/dao/impl/UserInfoDAOImpl.java
@@ -1,19 +1,150 @@
 package org.service.desk.dao.impl;
 
-import org.service.desk.entities.models.User;
-
 import java.util.List;
+
+import org.service.desk.entities.dao.UserInformationRepo;
+import org.service.desk.entities.models.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 
 //Custom methods available for DB interaction and retreival of data.
-public interface UserInfoDAOImpl{
+public class UserInfoDAOImpl implements UserInformationRepo {
+
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findAll(Sort arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> findAll(Example<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> findAll(Example<S> arg0, Sort arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> S insert(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> insert(Iterable<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> save(Iterable<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<User> findAll(Pageable arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Iterable<? extends User> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean exists(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterable<User> findAll(Iterable<String> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findOne(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> S save(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> long count(Example<S> arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends User> boolean exists(Example<S> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <S extends User> Page<S> findAll(Example<S> arg0, Pageable arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> S findOne(Example<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
 
 
-
-class UserCustomRepoImpl implements UserInfoDAOImpl {
-
-	
-
-}
--- desk/src/main/java/org/service/desk/entities/dao/UserInformationRepo.java
+++ desk/src/main/java/org/service/desk/entities/dao/UserInformationRepo.java
@@ -1,13 +1,11 @@
 package org.service.desk.entities.dao;
 
 
-import org.service.desk.dao.impl.UserInfoDAOImpl;
 import org.service.desk.entities.models.User;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
-public interface UserInformationRepo extends MongoRepository<User, String>, UserInfoDAOImpl{
+public interface UserInformationRepo extends MongoRepository<User, String> {
 	User findByUsername(String username);
 }
 
